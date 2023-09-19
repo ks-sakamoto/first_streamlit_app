202d19efa1bbb4012989c62ed4e3cd844c8cfa33
ks-sakamoto <ks.netgit@gmail.com>
2023-09-19 14:26:36 +0900

    revise text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Favorites")
-streamlit.text("🥣a 3 & Blueberry Oatmeal")
-streamlit.text("  �🍞Kale, Spinach & Rocket Smoothie")
-streamlit.text(" 🐔rd-Boiled Free-Range Egg")
-streamlit.text(' 🥑Avocado Toast🍞')
-
+streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔Hard-Boiled Free-Range Egg")
+streamlit.text('🥑🍞Avocado Toast')
