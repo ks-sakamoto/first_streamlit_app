5718b3b47cb57ea7aa333996d14ec77b94fdc606
ks-sakamoto <ks.netgit@gmail.com>
2023-09-19 14:24:23 +0900

    add menu, and icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣a 3 & Blueberry Oatmeal")
+streamlit.text("  �🍞Kale, Spinach & Rocket Smoothie")
+streamlit.text(" 🐔rd-Boiled Free-Range Egg")
+streamlit.text(' 🥑Avocado Toast🍞')
+
