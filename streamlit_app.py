86829a6c3d5255faeac813dfb20e58f305ba6e3e
ks-sakamoto <ks.netgit@gmail.com>
2023-09-19 14:55:02 +0900

    set-index

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
-import streamlit
 import pandas
-
+import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -8,15 +7,17 @@ streamlit.header("Breakfast Favorites")
 streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔Hard-Boiled Free-Range Egg")
-streamlit.text('🥑🍞Avocado Toast')
+streamlit.text("🥑🍞Avocado Toast")
 
-streamlit.header('🍌🥭 自分でフルーツスムージーを作ろう 🥝🍇')
+streamlit.header("🍌🥭 自分でフルーツスムージーを作ろう 🥝🍇")
 
-my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = pandas.read_csv(
+    "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+)
+my_fruit_list = my_fruit_list.set_index("Fruit")
 
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect('Pick some fruites:', my_fruit_list.index.to_list())
+streamlit.multiselect("Pick some fruites:", my_fruit_list.index.to_list())
 
 # display the table on the page
 streamlit.dataframe(my_fruit_list)
