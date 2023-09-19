6a6aa72cbb63b94c12afdd1d456bf4d13a1cbe82
ks-sakamoto <ks.netgit@gmail.com>
2023-09-19 14:27:44 +0900

    add header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔Hard-Boiled Free-Range Egg")
 streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 自分でフルーツスムージーを作ろう 🥝🍇')
